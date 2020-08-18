18caba8c8a483641352ca6961c1c4b1a6d6cbc84
José Antonio Riaza Valverde <riazavalverde@gmail.com>
2020-08-18 23:02:02 +0200

    added triangle and saw waves

--- Data/Signal.hs
+++ Data/Signal.hs
@@ -1,6 +1,6 @@
 module Data.Signal(
     Rate, Amplitude, Frecuency, Phase, Time, Signal(..),
-    sineWave, squareWave,
+    sineWave, squareWave, triangleWave, sawWave,
     takeZero, mapSignal, time, decay,
     ctrRate, audRate,
     zero,
@@ -40,7 +40,6 @@ type Frecuency = Double
 type Phase = Double
 
 -- | Time
-type Seconds = Double
 type Time = Double
 
 -- | Signal
@@ -82,10 +81,38 @@ squareWave :: Amplitude -> Phase -> Frecuency -> Signal
 squareWave a phi f = Signal (\rate ->
     let rate' = fromIntegral rate
     in map (\t -> a * signum (sin (2*pi*f*(t/rate') + phi))) [0..])
-              
+
+-- | triangleWave
+-- A triangle wave is a non-sinusoidal waveform named for its triangular shape.
+-- It is a periodic, piecewise linear, continuous real function. 
+-- Its form as a function of time t is: 
+--
+-- y(t) = 2*A / pi * asin(sin(2*pi*f*t + phi))
+--
+-- where A is the amplitude, f is the frecuency and phi is the phase.
+triangleWave :: Amplitude -> Phase -> Frecuency -> Signal
+triangleWave a phi f = Signal (\rate ->
+    let rate' = fromIntegral rate
+    in map (\t -> (2*a / pi) * asin (sin (2*pi*f*(t/rate') + phi))) [0..])
+
+-- | sawWave
+-- The sawtooth wave is a kind of non-sinusoidal waveform. The convention is
+-- that a sawtooth wave ramps upward and then sharply drops. In a reverse
+-- (or inverse) sawtooth wave, the wave ramps downward and then sharply rises.
+-- Its form as a function of time t is: 
+--
+-- y(t) = -2*A * ((t+phi)*f - floor(0.5+(t+phi)*f))
+--
+-- where A is the amplitude, f is the frecuency and phi is the phase.
+sawWave :: Amplitude -> Phase -> Frecuency -> Signal
+sawWave a phi f = Signal (\rate ->
+    let rate' = fromIntegral rate
+    in map (\t -> 2 * a * (((t+phi)/rate')*f - fromIntegral(floor (0.5 + ((t+phi)/rate')*f)))) [0..])
+
 
 -- | SIGNAL COMBINATORS
 
+-- | takeZero
 takeZero :: Int -> [Double] -> [Double]
 takeZero n xs = take (takeZero' n (drop n xs)) xs
     where takeZero' n [] = n
@@ -106,12 +133,12 @@ mapSignal g (Signal f) = Signal (\rate -> g rate (f rate))
 
 -- | time
 -- Take the first n seconds of a signal.
-time :: Seconds -> Signal -> Signal
+time :: Time -> Signal -> Signal
 time t = mapSignal (\rate -> takeZero (round (t * fromIntegral rate)))
 
 -- | decay
 -- Decay the signal the last n seconds.
-decay :: Seconds -> Signal -> Signal
+decay :: Time -> Signal -> Signal
 decay t (Signal f) = Signal (\rate ->
     let xs = f rate
         m = round (t * fromIntegral rate)
--- examples/titanic.hs
+++ examples/titanic.hs
@@ -2,7 +2,7 @@ import Data.WAVE(putWAVEFile)
 import Data.Signal
 
 titanic :: [Signal]
-titanic = map ((decay 0.3) . (time 0.5) . ($ (sineWave 1 0)))
+titanic = map ((decay 0.3) . (time 0.5) . ($ (sawWave 1 0)))
     [f,f,f,f,e,f,zero,f,e,f,zero,g,a,g]
 
 main :: IO ()
